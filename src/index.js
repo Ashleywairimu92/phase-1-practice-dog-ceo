b3e4a58692ad8dd03d5b4ed52e4f881a450495a3
Ashleywairimu92 <ashleywairimu18@gamil.com>
2024-07-21 00:11:53 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 console.log('%c HI', 'color: firebrick')
+describe("Array", () => {
+    describe("when empty", () => {
+      it("should have length 0", () => {
+        const arr = [];
+        if (arr.length !== 0) {
+          throw new Error("Array length is not 0");
+        }
+      });
+    });
+  });
+  
